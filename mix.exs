b39885e45a3cf89fab56406659056c922c7baa35
Gearnode <friminb@gmail.com>
2017-05-13 12:27:58 +0200

    Fix not fixed dependencies management
    Use hex.pm instead github.com to install dependencies.
    Use lock version to have more control on dependencies versioning.
    
    Add credo dependencies to improve code style, this dependence is
    only used in dev and test environments and this dependence is
    optional.

--- mix.exs
+++ mix.exs
@@ -2,23 +2,21 @@ defmodule Sidekiq.Mixfile do
   use Mix.Project
 
   def project do
-    [ app: :exsidekiq,
+    [app: :exsidekiq,
       version: "0.0.2",
       elixir: ">= 1.2.5",
-      deps: deps ]
+      deps: deps()]
   end
 
-  # Configuration for the OTP application
   def application do
-    [ applications: [:eredis, :crypto],
-      mod: { Sidekiq, [] },
-      registered: [Sidekiq.Server] ]
+    [applications: [:eredis, :crypto],
+     mod: {Sidekiq, []},
+     registered: [Sidekiq.Server]]
   end
 
-  # Returns the list of dependencies in the format:
-  # { :foobar, "~> 0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
-    [ { :eredis, github: "wooga/eredis" },
-      { :jiffy, github: "davisp/jiffy" } ]
+    [{:eredis, "~> 1.0"},
+      {:jiffy, "~> 0.14.11"},
+      {:credo, "~> 0.7.4", only: [:dev, :test], optional: true}]
   end
 end
